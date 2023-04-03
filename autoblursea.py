a026bbc448dade9a1a41bba4e2f1f0a63b2b809f
thanh99dev <thanhpham99dev@gmail.com>
2023-04-03 19:15:09 +0700

    stablev3

--- autoblursea.py
+++ autoblursea.py
@@ -1,7 +1,6 @@
 from selenium import webdriver
 from selenium.webdriver.common.keys import Keys
 from selenium.webdriver.common.by import By
-from selenium.webdriver.support.wait import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
 import csv
 import secrets
@@ -9,21 +8,23 @@ from eth_account import Account
 from datetime import datetime
 from datetime import date
 import time
-
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.common.exceptions import TimeoutException
 
 
 # Var
-
+delay = 20
 ref = 'https://blursea.io/'
 # webrd = 'https://vanity-eth.tk/'
 ii = 1
 h = 0
-check0 = '#withdraw'
-check1 = '//*[@id="airdrop-form"]/div/input'
-check2 = '//*[@id="airdrop-form"]/div/button'
-check3 = '//*[@id="airdrop-block"]/div/div[1]/span[2]'
-check4 = '//*[@id="airdrop-form"]/div/input'
-check5 = '//*[@id="airdrop-form"]/div/button'
+
+check0 = '//*[@id="airdrop-form"]/div/input'
+check1 = '//*[@id="airdrop-form"]/div/button'
+check2 = '#airdrop-block > div.col-12.col-sm-10.col-md-9.col-lg-8 > div.row.mt-4.text-center.justify-content-between > div:nth-child(2) > div.heading-h5.text-primary'
+check02 = '#airdrop-block > div.col-12.col-sm-10.col-md-9.col-lg-8 > div.text-break > span.text-primary.copy'
+check3 = '//*[@id="airdrop-form"]/div/input'
+check4 = '#withdraw'
 # Time
 today = date.today()
 day = today.day
@@ -55,6 +56,8 @@ def add_file(file, text):
     else:
         tk = tk + '\n'+ text
         write_file(file, tk)
+def wait (b,a):
+    WebDriverWait(driver, delay).until(EC.presence_of_element_located((b, a)))
 
 # Auto programming
 
@@ -66,73 +69,101 @@ with open('data.csv') as f:
         for i in reader:
             swap = l[h][1]
             swap2 = l[h][0]
-            print(swap)
-            print(swap2)
+            while True:
+                try:
+                    print(swap)
+                    print(swap2)
+                    print("Dang Auto Cho Wallet: ",swap)
+                    h += 1
+                    driver = webdriver.Chrome(executable_path="C:/Users/lehuy/Documents/cd/chromedriver.exe")
+                    driver.get(ref)
+                    wait(By.XPATH,check1)
+                    cs1 = driver.find_element(By.XPATH,check0)
+                    time.sleep(1)
+                    cs1.send_keys(swap)
+                    time.sleep(1)
+                    ip2 = driver.find_element(By.XPATH, check1)
+                    time.sleep(1)
+                    driver.execute_script("arguments[0].click();", ip2)
+                    wait(By.CSS_SELECTOR,check02)
+                    ii1 = driver.find_element(By.CSS_SELECTOR,check2)
+                    print(ii1.text)
+                    time.sleep(1)
+                    iiok = 50 - int((ii1.text))
+                    print(iiok)
+                    ii11 = ii1.text
+                    time.sleep(1)
+                    refok = driver.find_element(By.CSS_SELECTOR,check02)
+                    print('check02',refok.text)
+                    print('check iiok',iiok)
+                    write = open('data1.txt','w')
+                    write.write(refok.text)
+                    write.close()
 
-            print("Dang Auto Cho Wallet: ",swap)
-            h += 1
-            time.sleep(1)
-            driver = webdriver.Chrome(executable_path="C:/Users/lehuy/Documents/cd/chromedriver.exe")
-            driver.get(ref)
-            time.sleep(1)
-            print('ok 1')
-            cs1 = driver.find_element(By.XPATH,check1)
-            cs1.send_keys(swap)
-            time.sleep(1)
-            ip2 = driver.find_element(By.XPATH, check5)
-            time.sleep(1)
-            driver.execute_script("arguments[0].click();", ip2)
-            time.sleep(2)
-            ii1 = driver.find_element(By.CSS_SELECTOR,'#airdrop-block > div.col-12.col-sm-10.col-md-9.col-lg-8 > div.row.mt-4.text-center.justify-content-between > div:nth-child(2) > div.heading-h5.text-primary')
-            print(ii1.text)
-            iiok = 50 - int((ii1.text))
-            print(iiok)
-            ii11 = ii1.text
-            
-            if int(ii1.text) == 50:
-                 okwd = driver.find_element(By.CSS_SELECTOR, check0)
-                 driver.execute_script("arguments[0].click();", okwd)
+                    driver.close()
 
-                 print('Da Click Withdraw!!!')
-            time.sleep(2)
-           
-            
-            refok = driver.find_element(By.CSS_SELECTOR,'#airdrop-block > div > div.text-break > span.text-primary.copy')
-            print("ok2")
-            time.sleep(1)
-            for ii in range(iiok):
-                key = "0x" + secrets.token_hex(32)
-                address = Account.from_key(key).address
-
-                print('Address: ' + str(address) + '|' + str(key))
-                add_file('wallet.txt',key+'|'+address)
-
-                driver = webdriver.Chrome(executable_path="C:/Users/lehuy/Documents/cd/chromedriver.exe")
-                driver.get(refok.text) 
-                time.sleep(1)
-
-                ip = driver.find_element(By.XPATH, check4)
-                ip.send_keys(address)
-                time.sleep(1)
-                ip3 = driver.find_element(By.XPATH, check5)
-                driver.execute_script("arguments[0].click();", ip3)
-                time.sleep(2)
-                print("Dang Auto Lan Thu " + str(ii) + " Cho Wallet:" + str(swap))
-                #  Report Result
-            time.sleep(1)
-            print('ok 3')
-            time.sleep(3)
-            now2 = datetime.now()
-            timenow2 = now2.strftime("%d/%m/%Y, %H:%M:%S")
-            if ii == iiok:
-                f = open('log.txt',"a")
-                f.write('\n'+str(ii11) +' With ADDRESS : ' + str(swap) + '|' + str(swap2) + '\n At Time: ' + str(timenow2))
-                f.close()
-            else:
-                f = open('log.txt',"a")
-                f.write('\n' + str(ii11)+ ' With ADDRESS : ' + str(swap) + '|' + str(swap2) + '\n At Time: ' + str(timenow2))
-                f.close()
-
-                print("ok 4")
-        print("Auto End")
-        add_file('wallet.txt','\n----------------------------------------------------------------------------------------------------------')
+                    read = open('data1.txt','r')
+                    refokok = read.read()
+                        
+                    
+                    for ii in range(iiok):
+                        key = "0x" + secrets.token_hex(32)
+                        address = Account.from_key(key).address
+                        print('Address: ' + str(address) + '|' + str(key))
+                        add_file('wallet.txt',key+'|'+address)
+                        address2 = address
+                        while True:
+                            try:
+                                driver = webdriver.Chrome(executable_path="C:/Users/lehuy/Documents/cd/chromedriver.exe")
+                                driver.get(refokok)
+                                wait(By.XPATH,check1) 
+                                ip = driver.find_element(By.XPATH, check3)
+                                ip.send_keys(address2)
+                                # time.sleep(2)
+                                ip3 = driver.find_element(By.XPATH, check1)
+                                time.sleep(1)
+                                driver.execute_script("arguments[0].click();", ip3)
+                                time.sleep(5)
+                                
+                            except:
+                                continue
+                            break
+                        print("Dang Auto Lan Thu " + str(ii) + " Cho Wallet:" + str(swap))
+                        driver.quit()
+                        # time.sleep(2)
+                        #  Report Result
+                    
+                        
+                    print('ok 3')
+                    
+                    now2 = datetime.now()
+                    timenow2 = now2.strftime("%d/%m/%Y, %H:%M:%S")
+                    if iiok == 0:
+                        driver = webdriver.Chrome(executable_path="C:/Users/lehuy/Documents/cd/chromedriver.exe")
+                        driver.get(ref)
+                        wait(By.XPATH,check1)
+                        print('ok 4')
+                        cs1ok = driver.find_element(By.XPATH,check0)
+                        cs1ok.send_keys(swap)
+                        time.sleep(1)
+                        ip2ok = driver.find_element(By.XPATH, check1)
+                        driver.execute_script("arguments[0].click();", ip2ok)
+                        wait(By.CSS_SELECTOR,check4)
+                        okwdok = driver.find_element(By.CSS_SELECTOR, check4)
+                        time.sleep(1)
+                        driver.execute_script("arguments[0].click();", okwdok)
+                        driver.quit()
+                        print('Da Click Withdraw!!!')
+                        f = open('log.txt',"a")
+                        f.write('\n'+str(ii11) +' With ADDRESS : ' + str(swap) + '|' + str(swap2) + '\n At Time: ' + str(timenow2))
+                        f.close()
+                    else:
+                        f = open('log.txt',"a")
+                        f.write('\n' + str(ii11)+ ' Fail With ADDRESS : ' + str(swap) + '|' + str(swap2) + '\n At Time: ' + str(timenow2))
+                        f.close()
+                        print("ok 5")
+                except:
+                     continue
+                break
+            print("Auto End")
+            add_file('wallet.txt','\n----------------------------------------------------------------------------------------------------------')
--- autoblurseav2.py
+++ autoblurseav2.py
@@ -1,17 +1,28 @@
 import requests
+from bs4 import BeautifulSoup
+import time
+from datetime import datetime
+from datetime import date
+import json
 
 
+
+
+
+
+
+# Header
 header = {
-    'accept': 'text/html, */*; q=0.01',
+    "accept": 'text/html, */*; q=0.01',
     # 'accept-encoding': 'gzip, deflate, br',
     'accept-language': 'en-US,en;q=0.9',
-    'content-length': '629',
+    'content-length': '641',
     'content-type': 'application/x-www-form-urlencoded; charset=UTF-8',
-    'origin': 'https://blursea.io',
-    'referer': 'https://blursea.io/',
+    'origin': 'https://ethereumshanghai.com',
+    'referer': 'https://ethereumshanghai.com/',
     'sec-ch-ua': '"Google Chrome";v="111", "Not(A:Brand";v="8", "Chromium";v="111"',
     'sec-ch-ua-mobile': '?0',
-    'sec-ch-ua-platform': '"Windows"',
+    'sec-ch-ua-platform': "Windows",
     'sec-fetch-dest': 'empty',
     'sec-fetch-mode': 'cors',
     'sec-fetch-site': 'same-origin',
@@ -20,8 +31,47 @@ header = {
     'x-requested-with': 'XMLHttpRequest'
 }
 
+# Time
+today = date.today()
+day = today.day
+year = today.year
+now1 = datetime.now()
+timenow1 = now1.strftime("%d/%m/%Y, %H:%M:%S")
+f = open('log.txt',"a")
+f.write('\n-------------------------------------------------------------------\nMission starts at ' + str(timenow1))
+f.close()
 
-r = requests.get('http://blursea.io',headers=header)
+# Variable & Function
+url = 'https://ethereumshanghai.com/airdrop/submit'
 
-print(r.status_code)
-print(r.json)
+data1 = {'address': 0xf04aaE0981d01daf2d2F57E9162e6c8399668796}
+ 
+
+def read_file(a):
+    try:
+            file = open(a, encoding='utf-8')
+    except FileNotFoundError:
+         file = open(a, "a+", encoding='utf-8')
+    sst = file.read()
+    return sst
+
+def write_file(a,b):
+        file = open(a, 'w', encoding='utf-8')
+        file.write(b)
+        file.close()
+
+def add_file(file, text):
+    tk = read_file(file)
+    if tk =="":
+        tk = text
+        write_file(file, tk)
+    else:
+        tk = tk + '\n'+ text
+        write_file(file, tk)
+
+#  Execute
+submit1 = requests.post(url,headers=header,data=data1)
+print(submit1.status_code)
+print(submit1.text)
+
+# //*[@id="airdrop-form"]/div[2]/div
